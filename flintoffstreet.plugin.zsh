7e51ac326d560262cfb0738871cf029f6dd47020
John Layton <johnstewartlayton@gmail.com>
2021-07-29 12:35:49 +1000

    Update

--- flintoffstreet.plugin.zsh
+++ flintoffstreet.plugin.zsh
@@ -4,6 +4,9 @@ function flintoffstreet-add-auto-to-path() {
   if [[ -d bin ]]; then
     export PATH="./bin:$PATH"
   fi
+  if [[ -d script ]]; then
+    export PATH="./script:$PATH"
+  fi
 }
 
 add-zsh-hook chpwd flintoffstreet-add-auto-to-path
