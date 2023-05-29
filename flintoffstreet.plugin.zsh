bb5517667caaddbcfb29d5faed9e7d7ad262dfd9
John Layton <johnstewartlayton@gmail.com>
2023-05-29 12:56:35 +1000

    update scripts

--- flintoffstreet.plugin.zsh
+++ flintoffstreet.plugin.zsh
@@ -11,25 +11,27 @@ function flintoffstreet-add-auto-to-path() {
 
 add-zsh-hook chpwd flintoffstreet-add-auto-to-path
 
-function gradle-or-gradlew() {
-  # taken from https://github.com/gradle/gradle-completion
-  local dir="$PWD" project_root="$PWD"
-  while [[ "$dir" != / ]]; do
-    if [[ -f "$dir/settings.gradle" || -f "$dir/settings.gradle.kts" || -f "$dir/gradlew" ]]; then
-      project_root="$dir"
-      break
-    fi
-    dir="${dir:h}"
-  done
+#function gradle-or-gradlew() {
+#  # taken from https://github.com/gradle/gradle-completion
+#  local dir="$PWD" project_root="$PWD"
+#  while [[ "$dir" != / ]]; do
+#    if [[ -f "$dir/settings.gradle" || -f "$dir/settings.gradle.kts" || -f "$dir/gradlew" ]]; then
+#      project_root="$dir"
+#      break
+#    fi
+#    dir="${dir:h}"
+#  done
+#
+#  # if gradlew found, run it instead of gradle
+#  if [[ -f "$project_root/gradlew" ]]; then
+#    echo "executing gradlew instead of gradle"
+#    "$project_root/gradlew" "$@"
+#  else
+#    command gradle "$@"
+#  fi
+#}
+#
+#alias gradle=gradle-or-gradlew
+#compdef _gradle gradle-or-gradlew
 
-  # if gradlew found, run it instead of gradle
-  if [[ -f "$project_root/gradlew" ]]; then
-    echo "executing gradlew instead of gradle"
-    "$project_root/gradlew" "$@"
-  else
-    command gradle "$@"
-  fi
-}
 
-alias gradle=gradle-or-gradlew
-compdef _gradle gradle-or-gradlew
